bd7cb26f8b84624c08a10ed473ab3b3320c379dc
skyejung <107228623+skyejung@users.noreply.github.com>
2023-06-16 15:44:57 -0400

    FIRST COMMIT

---
+++ projecteuler.py
@@ -0,0 +1,14 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+# PROBLEM 1
+# Muliples of 3 or 5
+
+
+
+# PROBLEM 2
+# Even Fibonacci Numbers
+
+# PROBLEM 3
+# Largest Prime Factor
